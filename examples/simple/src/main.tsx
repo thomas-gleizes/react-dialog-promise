50f4b0400285b46bb88eccc0389a34458dbd36d4
thomas <thomasgleizes34@gmail.com>
2023-04-17 16:05:06 +0200

    switch

---
+++ examples/simple/src/main.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { DialogProvider } from "react-dialog-promise";
+
+import App from "./App";
+import "./global.css";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <DialogProvider>
+      <App />
+    </DialogProvider>
+  </React.StrictMode>
+);
